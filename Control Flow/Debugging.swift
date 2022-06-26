466c14427827f8768f3b2ee00fa8189d0bfc757f
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-26 09:33:59 -0700

    Create Debugging.swift

---
+++ Control Flow/Debugging.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Debugging:
+ 
+ 1. What did you want your app to do
+ 
+ 2. What happened instead
+ 
+ 3. What does your expectation depend upon
+ 
+ 4. How can we test the things our expectations depend on
+ 
+ 
+ */
