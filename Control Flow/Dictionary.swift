b84f87b3a250a793bfd8892fb97b77f2cc02cf3a
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-26 09:11:53 -0700

    Create Dictionary.swift

---
+++ Control Flow/Dictionary.swift
@@ -0,0 +1,32 @@
+var dict = ["Brewery": "a place where beer is made"]
+//              key             value (accompanying piece)
+
+var dict = ["Brewery": "a place where beer is made",
+            "Bakery" : "cake made"]
+
+// multiple keys and values
+
+
+var dict : [String: Int] =
+
+// declare datatype in a square bracket after declaring dictionary
+
+
+//Creating the Array
+
+var myArray = "Brewery", "Bakery"]
+
+// Retrieving from the array
+
+myArray[0]
+
+
+// Creating the Dictionary
+
+var dict : [String: Int] = ["Pine": 234232342,
+                            "Maple": 234622462]
+
+
+// Retrievinng from the dictionary
+
+dict["Pine"]
