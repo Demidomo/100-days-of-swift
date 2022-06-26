3df03b99840cc3f52596c994f53a87a0f55fa146
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-26 10:24:48 -0700

    Create MVC_Design_Pattern.swift

---
+++ Design Pattern and Code Structuring/MVC_Design_Pattern.swift
@@ -0,0 +1,33 @@
+/*
+ 
+ 
+ Design Pattern:
+ - minimize complexity
+ - Optomization
+ 
+ 
+ Examples:
+ - MVVM
+ - Memento
+ - Delegate
+ 
+ Requirement dependent for different design patterns
+ - Style based dependent (less lines vs. readable codes)
+ 
+ 
+ MVC Model View Controller
+ - Model: Separates the Data & Logic part of the code
+ - View: User Interface
+ - Controller: Mediator/Controller
+ 
+ 
+ When an action happens from the user
+ - View sends input events to controller,
+ - which makes a request to the model
+ - the Model sends the data back to the controller
+ - which modifes the view
+ 
+ The model and view never communicate to each other
+ - this helps when the userbase is bigger
+ - helps makes program much easier to understand 
+ */
