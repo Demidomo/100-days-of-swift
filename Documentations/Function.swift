d5ddd19843f52f41298476556125219978248c0a
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-25 23:08:49 -0700

    Update and rename Function to Function.swift

---
+++ Documentations/Function.swift
@@ -0,0 +1,82 @@
+//getMilk() vs. writing out every step
+
+//Creating the function: 
+//- func getMilk () { steps to do }
+
+//Calling the function
+//- getMilk()
+
+func greeting(){
+    print("Hello")
+}
+
+greeting()
+
+
+func greeting1() {
+    print("hello")
+    
+    var myName = "dumpster"
+}
+
+// The scope of myName is limited within greeting1
+// click on the curly braces to see the function fit
+
+// auto indent:
+// select all
+// editor
+// structure >>> Indent 
+
+
+
+//Functions with inputs 
+//- Depending on the condition, a certain output will be produced 
+
+
+
+
+
+var myAge = 12
+// myAge stores a whole number datatype
+// this variable is an int, (hold down option key)
+myAge = 13
+// myAge = "THreee"
+// this won't hold up because swift thinks this variable has an int datatype////
+
+
+
+// we can assign a datatype
+
+
+
+
+// Creating the function: 
+
+func getMilk (bottles Int) {
+//             name  datatype
+    var cost = bottles * 1.5
+}
+
+
+// Calling the function: 
+
+getMilk(bottles: 2)
+//     parameter, argument
+
+
+
+// argument is the value of the input 
+
+
+
+
+
+
+
+func greeting2(whoToGreet: String) {
+    print("Hello \(whoToGreet)")
+}
+
+greeting2(whoToGreet: "Angela") 
+
+
