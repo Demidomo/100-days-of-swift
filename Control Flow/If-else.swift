bb902448f8b8719f268204af565f4563160d0fcc
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-25 23:37:43 -0700

    Create If-else.swift

---
+++ Control Flow/If-else.swift
@@ -0,0 +1,31 @@
+if trafficLight == "green" {
+  go()
+} else if trafficLight == "yellow" {
+  useYourJudgement() 
+} else {
+  stop()
+}
+
+
+// these are conditional statements 
+
+func loveCalculator() {
+  let loveScore = Int.random(in: 0...100)
+  
+  if loveScore > 80 {
+    print("You love yourself like kanye loves kanye") // do a
+  } else if loveScore > 40 {  
+    print("coke and mentos")                          // do b
+  } else {
+    print("foreveralone"                              // do c
+         }
+
+}
+  
+loveCalculator()
+// random number generator
+  
+ // else if skips the step if the step does not meet condition
+  
+ 
+  
