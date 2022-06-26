ecf4b354dc3f0e9e309485c1e9c4f816def5f0f6
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-26 10:45:22 -0700

    Create Immutability.swift

---
+++ Design Pattern and Code Structuring/Immutability.swift
@@ -0,0 +1,12 @@
+/*
+ 
+ 
+ Structure is a blueprint
+ 
+ Property: What it's like
+ Method: What it can do
+ 
+ Immutability
+ - If we are making change to a struct property, we must include a mutating
+ 
+ */
