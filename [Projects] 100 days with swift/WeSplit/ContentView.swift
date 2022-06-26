5ca3ae5f87d907c8597cfd4e1fd802f12a1ea57f
Space_Garten <34130768+SpaceGarten@users.noreply.github.com>
2022-06-26 08:38:01 -0700

    Create ContentView.swift

---
+++ [Projects] 100 days with swift/WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+
+
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Matthew Mobi on 2022-03-10.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
